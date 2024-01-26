816a9bff41f503bc617e2d473fbed688f5502c6a
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-01-27 00:22:51 +0600

    bus ticket calculator condition

---
+++ Condition/ticket.js
@@ -0,0 +1,14 @@
+let age =62;
+let student=false;
+if(student==true){
+  console.log('You got 50% discount')
+}
+else if(age <= 10){
+  console.log('You are children, so No need ticket ')
+}
+else if(age>=60){
+  console.log("Uncle You got 20% discount")
+}
+else{
+  console.log("Give 800 tk fast")
+}
