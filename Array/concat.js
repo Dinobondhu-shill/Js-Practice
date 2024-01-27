cb3b3a7c5c104eb4d67eb7ba2f8f4ffa78226993
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-01-28 01:02:49 +0600

    concat added

---
+++ Array/concat.js
@@ -0,0 +1,7 @@
+let mark = [100, 70, 54, 64, 85,];
+let additionalMark =[90, 87,56,66]
+
+
+console.log(mark)
+console.log(additionalMark)
+console.log(mark.concat(additionalMark))
---
+++ Array/isArray.js
@@ -0,0 +1,16 @@
+// Checking if it's an Array
+// Instructions:
+
+// Create different variables, each containing either an array or a non-array value.
+
+// Now use isArray to check if each variable is an array.
+
+// Print a message to the console indicating whether each variable is an array or not.
+let marks = [82, 86, 86, 92, 75, 40, 60];
+let result = "failed"
+if(Array.isArray(result, marks)){
+  console.log('Yaaaahooo! got this array')
+}
+else{
+  console.log("muri kha")
+}
