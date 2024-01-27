1504503030f26d24b615ce1fee5cb22e43c405a4
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-01-27 23:07:49 +0600

    array task added

---
+++ Array/task-1.js
@@ -0,0 +1,7 @@
+let fruits = ['apple', 'banana', 'orange', 'watermelon', 'guava'];
+let newFruits = "Jambura"
+fruits[2] = newFruits
+
+console.log(fruits);
+console.log(fruits[3]);
+console.log(fruits[2])
