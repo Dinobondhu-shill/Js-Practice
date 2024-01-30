603b1cc165f2798ee05cef244c628f047ab46ac9
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-01-31 00:45:15 +0600

    odd and even number using while loop

---
+++ loop/while/odd-even.js
@@ -0,0 +1,15 @@
+// let num = 61;
+// while(num < 100){
+//   if(num % 2 ===1){
+//     console.log(num)
+//   }
+//   num++
+// }
+
+let num = 78;
+while(num <= 98){
+if(num % 2 ===0){
+  console.log(num)
+}
+num++
+}
