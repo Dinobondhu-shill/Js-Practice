6de27da3064d54716c3b2c0efe541dc5ad0c739b
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-02-03 00:46:19 +0600

    function practice task-1

---
+++ function/task-1.js
@@ -0,0 +1,14 @@
+
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply (num1, num2, num3, num4){
+  results = (num1*num2*num4*num3);
+  console.log(results);
+  return results;
+}
+const num1 = 4;
+const num2 = 45;
+const num3 = 96;
+const num4 = 74;
+const result = multiply(num1, num2, num3, num4);
+
